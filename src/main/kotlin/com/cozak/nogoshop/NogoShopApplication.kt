276699b11e4f73afe5c12b64189ca441e69bd6cf
신승혁 <Cozak92@gmail.com>
2022-02-23 21:34:41 +0900

    :tada: Begin

---
+++ src/main/kotlin/com/cozak/nogoshop/NogoShopApplication.kt
@@ -0,0 +1,11 @@
+package com.cozak.nogoshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NogoShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<NogoShopApplication>(*args)
+}
